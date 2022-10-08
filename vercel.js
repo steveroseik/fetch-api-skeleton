facb0a97c5f50af4f933bbc64edaddb4b418ce58
steveroseik <73657968+steveroseik@users.noreply.github.com>
2022-10-08 21:16:48 +0200

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,10 @@
+{
+   "version": 2,
+   "name": "runnerAPI",
+   "builds": [
+      { "src": "app.js", "use": "@vercel/node" }
+   ],
+   "routes": [
+      { "src": "/(.*)", "dest": "/app.js" }
+   ]
+}
